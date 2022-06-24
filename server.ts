c00ce5cc118d6967c2d19ce38c78abe77ca39182
kodeandre <andre.kodehode@gmail.com>
2022-06-24 23:06:31 +0200

    Added api folder and endpoint rework + switch refactor

--- server.ts
+++ server.ts
@@ -2,25 +2,24 @@ import * as http from 'http';
 import * as child_process from 'child_process';
 import * as dotenv from 'dotenv';
 dotenv.config();
+import * as fs from 'fs';
 
 const url = `${process.env.HOST}:${process.env.PORT}`;
 const exec = child_process.exec;
 
 const requestListener = ((req: any, res: any) => {
-
-  if (req.url === '/') {
+  switch (req.url) {
+  case '/':
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write('This is the home page');
     return res.end();
-  }
 
-  if (req.url === '/about') {
+  case '/about':
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write('This is the about page');
     return res.end();
-  }
 
-  if (req.url === '/shell') {
+  case '/shell':
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     return exec('sh ./scripts/main.sh', ((err, stdout) => {
       if (err) console.error(err);
@@ -28,20 +27,20 @@ const requestListener = ((req: any, res: any) => {
       res.write(`${stdout}`);
       res.end();
     }));
-  }
 
-  if (req.url === '/api/hello') {
+  case '/api/hello':
     res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.write(JSON.stringify({ message: 'Hello World' }));
-    return res.end();
-  }
+    return fs.readFile('./api/helloWorld.json', 'utf8', ((err, data) => {
+      if (err) console.error(err);
+      res.write(data);
+      return res.end();
+    }));
 
-  else {
+  default:
     res.writeHead(404, { 'Content-Type': 'text/html' });
     res.write('<h1>404 Page Not Found</h1>');
     return res.end();
   }
-
 });
 
 http.createServer(requestListener).listen(process.env.PORT, (() => console.log(`Your web server is running url: ${url}`)));
