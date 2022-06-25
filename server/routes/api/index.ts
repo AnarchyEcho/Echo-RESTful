2ec71f1194cc461fdecbcc5d4338e8ffc1a72e44
kodeandre <andre.kodehode@gmail.com>
2022-06-25 17:51:44 +0200

    Added sample json files

--- server/routes/api/index.ts
+++ server/routes/api/index.ts
@@ -2,9 +2,19 @@ import express from 'express';
 const router = express.Router();
 
 const helloWorld = require('./endpoints/helloWorld.json');
+const plants = require('./endpoints/plants.json');
+const cars = require('./endpoints/cars.json');
 
 router.get('/helloWorld', ((req: any, res: any) => {
   res.json(helloWorld);
+}));
+
+router.get('/plants', ((req: any, res: any) => {
+  res.json(plants);
+}));
+
+router.get('/cars', ((req: any, res: any) => {
+  res.json(cars);
 }));
 
 module.exports = router;
