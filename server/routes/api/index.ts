e0430a2a2a010d3258bbe784ddfca672064478b3
kodeandre <andre.kodehode@gmail.com>
2022-07-02 22:08:05 +0200

    Prepared shopping list json and endpoint

--- server/routes/api/index.ts
+++ server/routes/api/index.ts
@@ -4,9 +4,14 @@ const router = express.Router();
 const helloWorld = require('./endpoints/helloWorld.json');
 const plants = require('./endpoints/plants.json');
 const cars = require('./endpoints/cars.json');
+const shopList = require('./endpoints/shopping.json');
 
 router.get('/helloWorld', ((req: any, res: any) => {
   res.json(helloWorld);
+}));
+
+router.get('/shopping', ((req: any, res: any) => {
+  res.json(shopList);
 }));
 
 router.get('/plants', ((req: any, res: any) => {
